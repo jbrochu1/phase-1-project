1db531b936f47a374ae2d10648ccbfaeba7cc6f5
Paolo Garcia <pmatteo1108@yahoo.com>
2022-10-04 12:39:00 -0600

    recommit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6ece862459msh64649fba245c777p1c28dajsnb28c77bbfa56',
-		'X-RapidAPI-Host': 'sportspage-feeds.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '6ece862459msh64649fba245c777p1c28dajsnb28c77bbfa56',
+        'X-RapidAPI-Host': 'sportspage-feeds.p.rapidapi.com'
+    }
 };
 
 fetch('https://sportspage-feeds.p.rapidapi.com/games?odds=spread&league=MLB', options)
-	.then(response => response.json())
-	.then((response) => {
-        response.forEach
+    .then(response => response.json())
+    .then((response) => {
         const details = document.querySelector('#details')
         console.log(response);
         // details.innerHTML = JSON.stringify(response);
@@ -27,18 +25,6 @@ fetch('https://sportspage-feeds.p.rapidapi.com/games?odds=spread&league=MLB', op
         //console.log(response.results[0].schedule.date, todaysGame);
         gameTime.innerHTML = todaysGame.toLocaleDateString('en-EN') + ' ' + todaysGame.toLocaleTimeString('en-EN');
 
-
-
-
-
-
-        // const isoStr1 = gameTime;
-        // const date1 = new Date(isoStr1);
-        // console.log(date1);
-        // const date2 = new Date(gameTime.slice(0, -1));
-        // console.log(date2);
-
-
     })
-	.catch(err => console.error(err));
 
+    .catch(err => console.error(err));
