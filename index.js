e6ce82b162dfae94f0074a920273fb1a3eb1c4f2
Jason Brochu <jbrochu1@gmail.com>
2022-10-05 14:01:34 -0600

    watch button progress

--- index.js
+++ index.js
@@ -14,58 +14,65 @@ fetch('https://sportspage-feeds.p.rapidapi.com/games?odds=spread&league=MLB', op
     .then(response => response.json())
     .then(response => {
         console.log(response);
-        response.results.forEach(finalGame => {currentTeams(finalGame)})
-   })
+        response.results.forEach(finalGame => { currentTeams(finalGame) })
+    })
 
-        // details.innerHTML = JSON.stringify(response);
+// details.innerHTML = JSON.stringify(response);
 
-    const currentTeams = (finalGame) => {
-        const teams = document.createElement("p");
-        teams.innerHTML = finalGame.teams.away.team + ' @ ' + finalGame.teams.home.team;
-        scheduleBar.append(teams);
+const currentTeams = (finalGame) => {
+    const teams = document.createElement("p");
+    teams.innerHTML = finalGame.teams.away.team + ' @ ' + finalGame.teams.home.team;
+    scheduleBar.append(teams);
 
     teams.addEventListener('click', () => {
         console.log(finalGame);
         renderDetails(finalGame);
-        
-        })
+
+    })
+}
+
+const renderDetails = (finalGame) => {
+    //const element = document.createElement("div");
+    //console.log(gameDetails(finalGame));
+    console.log(finalGame);
+    const details = document.querySelector('#details');
+    details.innerHTML = finalGame.details.seasonType;
+    const odds = document.querySelector('#odds');
+    odds.innerHTML = finalGame.odds[0].spread.current.awayOdds + ' ' + finalGame.odds[0].spread.current.homeOdds;
+    const teams = document.querySelector('#teams');
+    teams.innerHTML = finalGame.teams.away.team + ' @ ' + finalGame.teams.home.team;
+    const venue = document.querySelector('#venue');
+    venue.innerHTML = finalGame.venue.name;
+    const gameTime = document.querySelector('#schedule');
+    gameTime.innerHTML = finalGame.schedule.date;
+    let todaysGame = new Date(finalGame.schedule.date);
+    //console.log(finalGame.schedule.date, todaysGame);
+    gameTime.innerHTML = todaysGame.toLocaleDateString('en-EN') + ' ' + todaysGame.toLocaleTimeString('en-EN');
+    const gameStatus = document.querySelector('#status');
+    gameStatus.innerHTML = finalGame.status;
+
+    if (finalGame.scoreboard) {
+        const currentScore = document.querySelector('#score');
+        currentScore.innerHTML = finalGame.scoreboard.score.away + ' - ' + finalGame.scoreboard.score.home;
+
     }
 
-        const renderDetails = (finalGame) => {
-            //const element = document.createElement("div");
-                //console.log(gameDetails(finalGame));
-                console.log(finalGame);
-                const details = document.querySelector('#details');
-                details.innerHTML = finalGame.details.seasonType;
-                const odds = document.querySelector('#odds');
-                odds.innerHTML = finalGame.odds[0].spread.current.awayOdds + ' ' + finalGame.odds[0].spread.current.homeOdds;
-                const teams = document.querySelector('#teams');
-                teams.innerHTML = finalGame.teams.away.team + ' @ ' + finalGame.teams.home.team;
-                const venue = document.querySelector('#venue');
-                venue.innerHTML = finalGame.venue.name;
-                const gameTime = document.querySelector('#schedule');
-                gameTime.innerHTML = finalGame.schedule.date;
-                let todaysGame = new Date(finalGame.schedule.date);
-                //console.log(finalGame.schedule.date, todaysGame);
-                gameTime.innerHTML = todaysGame.toLocaleDateString('en-EN') + ' ' + todaysGame.toLocaleTimeString('en-EN');
-                const gameStatus = document.querySelector('#status');
-                gameStatus.innerHTML = finalGame.status;
+    watchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        let p = document.createElement('p');
+        
+        p = teams;
+        watchBar.append(p);
 
-                if (finalGame.scoreboard) {
-                    const currentScore = document.querySelector('#score');
-                    currentScore.innerHTML = finalGame.scoreboard.score.away + ' - ' + finalGame.scoreboard.score.home;
+    })
 
-                }
-                
-                watchButton.addEventListener('click', addGame(teams))
-                    
-                }
+}
 
-        function addGame(teams) {
-            const p = document.createElement('p')
-            p.textContent = teams
-            watchBar.append(p);
-        }
+// function addGame(teams) {
+//     const p = document.createElement('p')
+//     p = teams;
+//     watchBar.append(p);
+// }
 
 
 
